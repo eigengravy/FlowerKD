27c1014fd6eb14b43d98aaa787c81c22a05e281f
Sarang S <email.sarangs@gmail.com>
2024-03-26 22:59:53 +0530

    last hope

--- imagenet-niid-fedntd-vgg.py
+++ imagenet-niid-fedntd-vgg.py
@@ -53,100 +53,124 @@ def apply_transforms(batch):
 
 class Net(nn.Module):
     def __init__(self, num_classes=200):
-        super(Net, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-        )
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-        )
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-        )
-        self.layer6 = nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-        )
-        self.layer7 = nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.layer8 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-        )
-        self.layer9 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-        )
-        self.layer10 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.layer11 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-        )
-        self.layer12 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-        )
-        self.layer13 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.fc = nn.Sequential(nn.Dropout(0.5), nn.Linear(2048, 2048), nn.ReLU())
-        self.fc1 = nn.Sequential(nn.Dropout(0.5), nn.Linear(2048, 512), nn.ReLU())
-        self.fc2 = nn.Sequential(nn.Linear(512, num_classes))
+        super(Net).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.conv8 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.conv9 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv10 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv11 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv12 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv13 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.fc = nn.Linear(2048, 2048)
+        self.fc1 = nn.Linear(2048, 512)
+        self.fc2 = nn.Linear(512, num_classes)
+
+        # self.layer3 = nn.Sequential(
+        #     nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(128),
+        #     nn.ReLU(),
+        # )
+        # self.layer4 = nn.Sequential(
+        #     nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(128),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(kernel_size=2, stride=2),
+        # )
+        # self.layer5 = nn.Sequential(
+        #     nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(256),
+        #     nn.ReLU(),
+        # )
+        # self.layer6 = nn.Sequential(
+        #     nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(256),
+        #     nn.ReLU(),
+        # )
+        # self.layer7 = nn.Sequential(
+        #     nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(256),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(kernel_size=2, stride=2),
+        # )
+        # self.layer8 = nn.Sequential(
+        #     nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(512),
+        #     nn.ReLU(),
+        # )
+        # self.layer9 = nn.Sequential(
+        #     nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(512),
+        #     nn.ReLU(),
+        # )
+        # self.layer10 = nn.Sequential(
+        #     nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(512),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(kernel_size=2, stride=2),
+        # )
+        # self.layer11 = nn.Sequential(
+        #     nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(512),
+        #     nn.ReLU(),
+        # )
+        # self.layer12 = nn.Sequential(
+        #     nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(512),
+        #     nn.ReLU(),
+        # )
+        # self.layer13 = nn.Sequential(
+        #     nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(512),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(kernel_size=2, stride=2),
+        # )
+        # self.fc = nn.Sequential(nn.Dropout(0.5), nn.Linear(2048, 2048), nn.ReLU())
+        # self.fc1 = nn.Sequential(nn.Dropout(0.5), nn.Linear(2048, 512), nn.ReLU())
+        # self.fc2 = nn.Sequential(nn.Linear(512, num_classes))
 
     def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.layer5(out)
-        out = self.layer6(out)
-        out = self.layer7(out)
-        out = self.layer8(out)
-        out = self.layer9(out)
-        out = self.layer10(out)
-        out = self.layer11(out)
-        out = self.layer12(out)
-        out = self.layer13(out)
-        out = out.reshape(out.size(0), -1)
-        out = self.fc(out)
-        out = self.fc1(out)
-        out = self.fc2(out)
-        return out
+        x = F.relu(F.batch_norm(self.conv1(x)))
+        x = F.max_pool2d(F.relu(F.batch_norm(self.conv2(x))), 2, 2)
+        x = F.relu(F.batch_norm(self.conv3(x)))
+        x = F.max_pool2d(F.relu(F.batch_norm(self.conv4(x))), 2, 2)
+        x = F.relu(F.batch_norm(self.conv5(x)))
+        x = F.relu(F.batch_norm(self.conv6(x)))
+        x = F.max_pool2d(F.relu(F.batch_norm(self.conv7(x))), 2, 2)
+        x = F.relu(F.batch_norm(self.conv8(x)))
+        x = F.relu(F.batch_norm(self.conv9(x)))
+        x = F.relu(F.batch_norm(self.conv10(x)))
+        x = F.relu(F.batch_norm(self.conv11(x)))
+        x = F.relu(F.batch_norm(self.conv12(x)))
+        x = F.max_pool2d(F.relu(F.batch_norm(self.conv13(x))), 2, 2)
+        x = x.view(-1, 2048)
+        x = F.relu(self.fc(F.dropout(x, 0.5)))
+        x = F.relu(self.fc1(F.dropout(x, 0.5)))
+        x = self.fc2(x)
+        return x
+        # out = self.layer1(x)
+        # out = self.layer2(out)
+        # out = self.layer3(out)
+        # out = self.layer4(out)
+        # out = self.layer5(out)
+        # out = self.layer6(out)
+        # out = self.layer7(out)
+        # out = self.layer8(out)
+        # out = self.layer9(out)
+        # out = self.layer10(out)
+        # out = self.layer11(out)
+        # out = self.layer12(out)
+        # out = self.layer13(out)
+        # out = out.reshape(out.size(0), -1)
+        # out = self.fc(out)
+        # out = self.fc1(out)
+        # out = self.fc2(out)
+        # return out
 
 
 def train(  # pylint: disable=too-many-arguments
