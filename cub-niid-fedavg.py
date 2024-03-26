1ef0d90edbab20938661cfe341b8e9529d659e42
Sarang S <email.sarangs@gmail.com>
2024-03-27 03:07:23 +0530

    grayscale fix

--- cub-niid-fedavg.py
+++ cub-niid-fedavg.py
@@ -48,6 +48,7 @@ def apply_transforms(batch):
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            transforms.Lambda(lambda x: x.repeat(3, 1, 1) if x.size(0) == 1 else x),
         ]
     )
     batch["image"] = [tfs(img) for img in batch["image"]]
