1d5d839009d02d9745fc55db845dc3deaf9f3bf8
Sarang S <email.sarangs@gmail.com>
2024-04-24 02:35:53 +0530

    fix

--- imagenet/imagenet-niid-vgg-cnn-fedduet-local.py
+++ imagenet/imagenet-niid-vgg-cnn-fedduet-local.py
@@ -59,16 +59,14 @@ class DistillNet(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.conv3 = nn.Conv2d(16, 32, 5)
-        self.fc1 = nn.Linear(32 * 9 * 9, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, num_classes)
+        self.fc1 = nn.Linear(2704, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 200)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = torch.flatten(x, 1)
+        x = x.view(-1, 2704)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
